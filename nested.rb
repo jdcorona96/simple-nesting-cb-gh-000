dba327690b519ae3161c74f5de605efb1824aed1
Joseph Corona <josed.hc96@gmail.com>
2018-11-16 21:22:38 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -108,7 +108,7 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-     programmer_hash[:alan_kay][:known_for] = "GUI"
+     #programmer_hash[:alan_kay][:known_for] = "GUI"
 
 
 end
@@ -130,6 +130,6 @@ def adding_to_dennis
         }
      }
 
-		# programmer_hash[:dennis_ritchie][:languages]
+		 programmer_hash[:dennis_ritchie][:languages]
 
 end
